e5b539fbe889049e58a8e5f245d4fe5e6b592d28
22-2025082-afk <22-2025082@edu.city.izumi.osaka.jp>
2026-06-30 13:47:06 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+// ====================================
+// Pokémon Champions Damage Calculator
+// Ver.0.1.0
+// ====================================
 
+const calculateButton = document.getElementById("calculateButton");
+
+const damageValue = document.getElementById("damageValue");
+const damagePercent = document.getElementById("damagePercent");
+const koCount = document.getElementById("koCount");
+const hpFill = document.querySelector(".hp-fill");
+
+calculateButton.addEventListener("click", () => {
+
+    // 仮の表示
+    damageValue.textContent = "72 ～ 85";
+
+    damagePercent.textContent = "34.7% ～ 41.0%";
+
+    koCount.textContent = "確定3発";
+
+    hpFill.style.width = "65%";
+
+});
